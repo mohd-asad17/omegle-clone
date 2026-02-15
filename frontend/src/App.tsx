87f943fcf55bf6a78ee5da263f97101b60d2825f
mohd-asad17 <asadn01702@gmail.com>
2026-02-16 00:07:50 +0530

    initialize the project and some frontend done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import { Landing } from "./components/Landing";
+import { Room } from "./components/Room";
+
+function App() {
+ 
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/room" element={<Room />} />
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
